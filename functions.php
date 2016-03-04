b35d5cb2f4a1497c92ba7a080a3b5b5004875ae9
Shobi P P <ppshobi@gmail.com>
2016-03-05 00:33:30 +0530

    Implemented the ordering process completely from the checkout page while logged in adding to order table and details to order id tabe

--- functions.php
+++ functions.php
@@ -48,5 +48,71 @@ function isuserloggedin(){
 	}
 }
 
+function placeorder(){
+	if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+		if (isset($_GET['placeorder'])) {
+			$error=0;
+			$userid=$_SESSION['farmercart_user_id'];
+			//shipment details
+			$title=$_GET['title'];
+			$name=$_GET['name'];
+			$addr=$_GET['addr1'] . "<br/>" . $_GET['addr2'];
+			$pin=$_GET['pin'];
+			$city=$_GET['city'];
+			$country=$_GET['country'];
+			$state=$_GET['state'];
+			$phone=$_GET['phone'];
+			//building shipto address from the get values
+			$shipto= $title . "." .$name . "<br/>";
+			$shipto.=$addr . "<br/>";
+			$shipto.=$city . " " .$state . "<br/>";
+			$shipto.=$country . "<br/>";
+			$shipto.=$pin . "<br/>";
+			$shipto.=$phone;
+
+			//user id
+			$userid=$_SESSION['farmercart_user_id'];
+			$totalcost=$_SESSION['totalcost'];
+			$sql="INSERT INTO orders (orderid,userid,totalcost,shipto,orderstatus,orderdate)VALUES('','$userid','$totalcost','$shipto','0',CURDATE())";
+			$result=mysqlexec($sql);
+			if ($result) {
+				//inserted details to order table then insert to order details table
+				$sql="SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'farmercart' AND TABLE_NAME = 'orders'";
+				$result=mysqlexec($sql);
+
+				$row=mysqli_fetch_array($result);
+				//finding last inserted order id
+				$orderid=$row[0];
+
+				//Getting product details from shopping cart session;
+				if (isset($_SESSION['cart'])) {
+					$items=$_SESSION['cart'];
+					foreach($items as $key => $product){
+						$prodid=$product['id'];
+						$price=$product['price'];
+						$qty=$product['qty'];
+					$sql2="INSERT INTO orderdetails(detailid,orderid,productid,price,qty)VALUES('','$orderid','$prodid','$price','$qty')";
+					$result2=mysqlexec($sql2);
+					if (!$result2) {
+						$error=1;
+						break;
+					}
+					
+					}// end of for each loop
+
+				}
+			}
+		
+			if ($error==1) {
+				echo "<script>alert(\"Erroir When inserting to order details\")</script>";
+			}else{
+				echo "<script>alert(\"Your Order Placed Successfully\")</script>";
+				unset($_SESSION['cart']);
+			}
+		}
+	}
+
+}//end of placeorder function
+
 
 ?>
---
+++ includes/footer.php
@@ -0,0 +1,237 @@
+<footer id="footer">
+				<div class="footer_top_part">
+					<div class="container">
+						<div class="row clearfix">
+							<div class="col-lg-3 col-md-3 col-sm-3 m_xs_bottom_30">
+								<h3 class="color_light_2 m_bottom_20">About</h3>
+								<p class="m_bottom_25">Ut pharetra augue nec augue. Nam elit agna, endrerit sit amet, tincidunt ac, viverra sed, nulla. Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque.</p>
+								<!--social icons-->
+								<ul class="clearfix horizontal_list social_icons">
+									<li class="facebook m_bottom_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">Facebook</span>
+										<a href="#" class="r_corners t_align_c tr_delay_hover f_size_ex_large">
+											<i class="fa fa-facebook"></i>
+										</a>
+									</li>
+									<li class="twitter m_left_5 m_bottom_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">Twitter</span>
+										<a href="#" class="r_corners f_size_ex_large t_align_c tr_delay_hover">
+											<i class="fa fa-twitter"></i>
+										</a>
+									</li>
+									<li class="google_plus m_left_5 m_bottom_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">Google Plus</span>
+										<a href="#" class="r_corners f_size_ex_large t_align_c tr_delay_hover">
+											<i class="fa fa-google-plus"></i>
+										</a>
+									</li>
+									<li class="rss m_left_5 m_bottom_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">Rss</span>
+										<a href="#" class="r_corners f_size_ex_large t_align_c tr_delay_hover">
+											<i class="fa fa-rss"></i>
+										</a>
+									</li>
+									<li class="pinterest m_left_5 m_bottom_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">Pinterest</span>
+										<a href="#" class="r_corners f_size_ex_large t_align_c tr_delay_hover">
+											<i class="fa fa-pinterest"></i>
+										</a>
+									</li>
+									<li class="instagram m_left_5 m_bottom_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">Instagram</span>
+										<a href="#" class="r_corners f_size_ex_large t_align_c tr_delay_hover">
+											<i class="fa fa-instagram"></i>
+										</a>
+									</li>
+									<li class="linkedin m_bottom_5 m_sm_left_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">LinkedIn</span>
+										<a href="#" class="r_corners f_size_ex_large t_align_c tr_delay_hover">
+											<i class="fa fa-linkedin"></i>
+										</a>
+									</li>
+									<li class="vimeo m_left_5 m_bottom_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">Vimeo</span>
+										<a href="#" class="r_corners f_size_ex_large t_align_c tr_delay_hover">
+											<i class="fa fa-vimeo-square"></i>
+										</a>
+									</li>
+									<li class="youtube m_left_5 m_bottom_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">Youtube</span>
+										<a href="#" class="r_corners f_size_ex_large t_align_c tr_delay_hover">
+											<i class="fa fa-youtube-play"></i>
+										</a>
+									</li>
+									<li class="flickr m_left_5 m_bottom_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">Flickr</span>
+										<a href="#" class="r_corners f_size_ex_large t_align_c tr_delay_hover">
+											<i class="fa fa-flickr"></i>
+										</a>
+									</li>
+									<li class="envelope m_left_5 m_bottom_5 relative">
+										<span class="tooltip tr_all_hover r_corners color_dark f_size_small">Contact Us</span>
+										<a href="#" class="r_corners f_size_ex_large t_align_c tr_delay_hover">
+											<i class="fa fa-envelope-o"></i>
+										</a>
+									</li>
+								</ul>
+							</div>
+							<div class="col-lg-3 col-md-3 col-sm-3 m_xs_bottom_30">
+								<h3 class="color_light_2 m_bottom_20">The Service</h3>
+								<ul class="vertical_list">
+									<li><a class="color_light tr_delay_hover" href="#">My account<i class="fa fa-angle-right"></i></a></li>
+									<li><a class="color_light tr_delay_hover" href="#">Order history<i class="fa fa-angle-right"></i></a></li>
+									<li><a class="color_light tr_delay_hover" href="#">Wishlist<i class="fa fa-angle-right"></i></a></li>
+									<li><a class="color_light tr_delay_hover" href="#">Vendor contact<i class="fa fa-angle-right"></i></a></li>
+									<li><a class="color_light tr_delay_hover" href="#">Front page<i class="fa fa-angle-right"></i></a></li>
+									<li><a class="color_light tr_delay_hover" href="#">Virtuemart categories<i class="fa fa-angle-right"></i></a></li>
+								</ul>
+							</div>
+							<div class="col-lg-3 col-md-3 col-sm-3 m_xs_bottom_30">
+								<h3 class="color_light_2 m_bottom_20">Information</h3>
+								<ul class="vertical_list">
+									<li><a class="color_light tr_delay_hover" href="#">About us<i class="fa fa-angle-right"></i></a></li>
+									<li><a class="color_light tr_delay_hover" href="#">New collection<i class="fa fa-angle-right"></i></a></li>
+									<li><a class="color_light tr_delay_hover" href="#">Best sellers<i class="fa fa-angle-right"></i></a></li>
+									<li><a class="color_light tr_delay_hover" href="#">Manufacturers<i class="fa fa-angle-right"></i></a></li>
+									<li><a class="color_light tr_delay_hover" href="#">Privacy policy<i class="fa fa-angle-right"></i></a></li>
+									<li><a class="color_light tr_delay_hover" href="#">Terms &amp; condition<i class="fa fa-angle-right"></i></a></li>
+								</ul>
+							</div>
+							<div class="col-lg-3 col-md-3 col-sm-3">
+								<h3 class="color_light_2 m_bottom_20">Newsletter</h3>
+								<p class="f_size_medium m_bottom_15">Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
+								<form id="newsletter">
+									<input type="email" placeholder="Your email address" class="m_bottom_20 r_corners f_size_medium full_width" name="newsletter-email">
+									<button type="submit" class="button_type_8 r_corners bg_scheme_color color_light tr_all_hover">Subscribe</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--copyright part-->
+				<div class="footer_bottom_part">
+					<div class="container clearfix t_mxs_align_c">
+						<p class="f_left f_mxs_none m_mxs_bottom_10">&copy; 2014 <span class="color_light">Farmercart</span>. All Rights Reserved.</p>
+						<ul class="f_right horizontal_list clearfix f_mxs_none d_mxs_inline_b">
+							<li><img src="images/payment_img_1.png" alt=""></li>
+							<li class="m_left_5"><img src="images/payment_img_2.png" alt=""></li>
+							<li class="m_left_5"><img src="images/payment_img_3.png" alt=""></li>
+							<li class="m_left_5"><img src="images/payment_img_4.png" alt=""></li>
+							<li class="m_left_5"><img src="images/payment_img_5.png" alt=""></li>
+						</ul>
+					</div>
+				</div>
+			</footer>
+		</div>
+		<!--social widgets-->
+		<ul class="social_widgets d_xs_none">
+			<!--facebook-->
+			<li class="relative">
+				<button class="sw_button t_align_c facebook"><i class="fa fa-facebook"></i></button>
+				<div class="sw_content">
+					<h3 class="color_dark m_bottom_20">Join Us on Facebook</h3>
+					<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fenvato&amp;width=235&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;header=false&amp;stream=false&amp;show_border=false&amp;appId=438889712801266" style="border:none; overflow:hidden; width:235px; height:258px;"></iframe>
+				</div>
+			</li>
+			<!--twitter feed-->
+			<li class="relative">
+				<button class="sw_button t_align_c twitter"><i class="fa fa-twitter"></i></button>
+				<div class="sw_content">
+					<h3 class="color_dark m_bottom_20">Latest Tweets</h3>
+					<div class="twitterfeed m_bottom_25"></div>
+					<a role="button" class="button_type_4 d_inline_b r_corners tr_all_hover color_light tw_color" href="https://twitter.com/fanfbmltemplate">Follow on Twitter</a>
+				</div>	
+			</li>
+			<!--contact form-->
+			<li class="relative">
+				<button class="sw_button t_align_c contact"><i class="fa fa-envelope-o"></i></button>
+				<div class="sw_content">
+					<h3 class="color_dark m_bottom_20">Contact Us</h3>
+					<p class="f_size_medium m_bottom_15">Lorem ipsum dolor sit amet, consectetuer adipis mauris</p>
+					<form id="contactform" class="mini">
+						<input class="f_size_medium m_bottom_10 r_corners full_width" type="text" name="cf_name" placeholder="Your name">
+						<input class="f_size_medium m_bottom_10 r_corners full_width" type="email" name="cf_email" placeholder="Email">
+						<textarea class="f_size_medium r_corners full_width m_bottom_20" placeholder="Message" name="cf_message"></textarea>
+						<button type="submit" class="button_type_4 r_corners mw_0 tr_all_hover color_dark bg_light_color_2">Send</button>
+					</form>
+				</div>	
+			</li>
+			<!--contact info-->
+			<li class="relative">
+				<button class="sw_button t_align_c googlemap"><i class="fa fa-map-marker"></i></button>
+				<div class="sw_content">
+					<h3 class="color_dark m_bottom_20">Store Location</h3>
+					<ul class="c_info_list">
+						<li class="m_bottom_10">
+							<div class="clearfix m_bottom_15">
+								<i class="fa fa-map-marker f_left color_dark"></i>
+								<p class="contact_e">8901 Marmora Road,<br> Glasgow, D04 89GR.</p>
+							</div>
+							<iframe class="r_corners full_width" id="gmap_mini" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=ru&amp;geocode=&amp;q=Manhattan,+New+York,+NY,+United+States&amp;aq=0&amp;oq=monheten&amp;sll=37.0625,-95.677068&amp;sspn=65.430355,129.814453&amp;t=m&amp;ie=UTF8&amp;hq=&amp;hnear=%D0%9C%D0%B0%D0%BD%D1%85%D1%8D%D1%82%D1%82%D0%B5%D0%BD,+%D0%9D%D1%8C%D1%8E-%D0%99%D0%BE%D1%80%D0%BA,+%D0%9D%D1%8C%D1%8E+%D0%99%D0%BE%D1%80%D0%BA,+%D0%9D%D1%8C%D1%8E-%D0%99%D0%BE%D1%80%D0%BA&amp;ll=40.790278,-73.959722&amp;spn=0.015612,0.031693&amp;z=13&amp;output=embed"></iframe>
+						</li>
+						<li class="m_bottom_10">
+							<div class="clearfix m_bottom_10">
+								<i class="fa fa-phone f_left color_dark"></i>
+								<p class="contact_e">800-559-65-80</p>
+							</div>
+						</li>
+						<li class="m_bottom_10">
+							<div class="clearfix m_bottom_10">
+								<i class="fa fa-envelope f_left color_dark"></i>
+								<a class="contact_e default_t_color" href="mailto:#">info@companyname.com</a>
+							</div>
+						</li>
+						<li>
+							<div class="clearfix">
+								<i class="fa fa-clock-o f_left color_dark"></i>
+								<p class="contact_e">Monday - Friday: 08.00-20.00 <br>Saturday: 09.00-15.00<br> Sunday: closed</p>
+							</div>
+						</li>
+					</ul>
+				</div>	
+			</li>
+		</ul>
+		<!--login popup-->
+		<div class="popup_wrap d_none" id="login_popup">
+			<section class="popup r_corners shadow">
+				<button class="bg_tr color_dark tr_all_hover text_cs_hover close f_size_large"><i class="fa fa-times"></i></button>
+				<h3 class="m_bottom_20 color_dark">Log In</h3>
+				<form>
+					<ul>
+						<li class="m_bottom_15">
+							<label for="username" class="m_bottom_5 d_inline_b">Username</label><br>
+							<input type="text" name="" id="username" class="r_corners full_width">
+						</li>
+						<li class="m_bottom_25">
+							<label for="password" class="m_bottom_5 d_inline_b">Password</label><br>
+							<input type="password" name="" id="password" class="r_corners full_width">
+						</li>
+						<li class="m_bottom_15">
+							<input type="checkbox" class="d_none" id="checkbox_10"><label for="checkbox_10">Remember me</label>
+						</li>
+						<li class="clearfix m_bottom_30">
+							<button class="button_type_4 tr_all_hover r_corners f_left bg_scheme_color color_light f_mxs_none m_mxs_bottom_15">Log In</button>
+							<div class="f_right f_size_medium f_mxs_none">
+								<a href="#" class="color_dark">Forgot your password?</a><br>
+								<a href="#" class="color_dark">Forgot your username?</a>
+							</div>
+						</li>
+					</ul>
+				</form>
+				<footer class="bg_light_color_1 t_mxs_align_c">
+					<h3 class="color_dark d_inline_middle d_mxs_block m_mxs_bottom_15">New Customer?</h3>
+					<a href="#" role="button" class="tr_all_hover r_corners button_type_4 bg_dark_color bg_cs_hover color_light d_inline_middle m_mxs_left_0">Create an Account</a>
+				</footer>
+			</section>
+		</div>
+		<button class="t_align_c r_corners tr_all_hover type_2 animate_ftl" id="go_to_top"><i class="fa fa-angle-up"></i></button>
+		<!--scripts include-->
+		<script src="js/jquery-2.1.0.min.js"></script>
+		<script src="js/jquery-ui.min.js"></script>
+		<script src="js/retina.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/waypoints.min.js"></script>
+		<script src="js/jquery.tweet.min.js"></script>
+		<script src="js/scripts.js"></script>
+	</body>
+</html>
